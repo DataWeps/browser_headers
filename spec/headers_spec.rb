284bdab84ffda347bf65fcf3c0d5119665e82a40
Jan Mosat <jmosat@gmail.com>
2016-03-24 10:12:49 +0100

    [MOD] use let instead of before in tests

--- spec/headers_spec.rb
+++ spec/headers_spec.rb
@@ -3,9 +3,9 @@ require 'browser_headers'
 require 'net/http'
 
 describe BrowserHeaders do
-  context 'get data from' do
-    before(:context) do
-      @headers = BrowserHeaders::Client.new(
+  context 'browser_headers gem' do
+    let(:headers) do
+      BrowserHeaders::Client.new(
         redis_host: '127.0.0.1',
         redis_port: '6379',
         redis_db: '0',
@@ -13,49 +13,51 @@ describe BrowserHeaders do
         login: 'admin',
         password: 'admin'
       )
-      @redis = Redis.new
-      @redis.del('headers')
     end
+    let(:redis) { Redis.new }
 
     it 'connects to redis' do
-      expect(@headers.ping).to eq('PONG')
+      expect(headers.ping).to eq('PONG')
     end
 
     it 'downloads headers' do
-      @headers.fetch_headers
-      expect(@redis.zcard('headers')).to be > 0
+      headers.fetch_headers
+      expect(redis.zcard('headers')).to be > 0
     end
 
     it 'returns correct number of headers' do
       number = 20
-      headers = @headers.get_headers(20)
-      expect(headers.size).to eq(number)
+      headers.fetch_headers
+      headers_list = headers.get_headers(20)
+      expect(headers_list.size).to eq(number)
     end
 
     it 'does not duplicate headers' do
-      @headers.fetch_headers
-      header_count = @redis.zcard('headers')
-      @headers.fetch_headers
-      header_count -= @redis.zcard('headers')
+      headers.fetch_headers
+      header_count = redis.zcard('headers')
+      headers.fetch_headers
+      header_count -= redis.zcard('headers')
       expect(header_count).to be 0
     end
 
     it 'updates time of last download' do
-      @headers.fetch_headers
-      first_date = @headers.instance_variable_get(:@headers)
+      headers.fetch_headers
+      first_date = headers.instance_variable_get(:@headers)
+                          .instance_variable_get(:@last_update)
+      headers.fetch_headers
+      second_date = headers.instance_variable_get(:@headers)
                            .instance_variable_get(:@last_update)
-      @headers.fetch_headers
-      second_date = @headers.instance_variable_get(:@headers)
-                            .instance_variable_get(:@last_update)
       expect(second_date).to be > first_date
     end
 
     it 'creates correct uri' do
-      @headers.fetch_headers
-      headers_helper = @headers.instance_variable_get(:@headers)
+      headers.fetch_headers
+      headers_helper = headers.instance_variable_get(:@headers)
       last_update = headers_helper.instance_variable_get(:@last_update)
       uri = headers_helper.make_uri
       expect(uri.query).to eq("since=#{last_update.strftime('%F')}")
     end
+
+    after(:each) { Redis.new.del('headers') }
   end
 end
